abe2b309adc94ad3b856959f0094c9de84c933e6
Aravind0211 <113758941+Aravind0211@users.noreply.github.com>
2024-06-25 12:29:39 +0530

    Update ProductDto.java

--- src/main/java/dev/dberenguer/dapr/product/dto/ProductDto.java
+++ src/main/java/dev/dberenguer/dapr/product/dto/ProductDto.java
@@ -2,11 +2,9 @@ package dev.dberenguer.dapr.product.dto;
 
 import lombok.Builder;
 import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 @Value
 @Builder
-@Jacksonized
 public class ProductDto {
     String code;
     String name;
